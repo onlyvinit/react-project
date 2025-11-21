fd1509021bda6e953b03139bf501fb5ef12c291a
onlyvinit <165870449+onlyvinit@users.noreply.github.com>
2025-11-21 16:47:30 +0530

    Update firebase.js

--- Awth/src/firebase.js
+++ Awth/src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "add your api key",
-  authDomain: "login-8131b.firebaseapp.com",
-  projectId: "login-8131b",
-  storageBucket: "login-8131b.firebasestorage.app",
-  messagingSenderId: "134169095453",
-  appId: "1:134169095453:web:6009b7b4c53380237945f4",
+  apiKey: "jrbierberivberdcccdbcv",
+  authDomain: "icvniejsdcscscrecvccd",
+  projectId: "dcvsdcscevevrev",
+  storageBucket: "wncwjecisdcsccsdwjceccc",
+  messagingSenderId: "13416909sdcsdcc5453",
+  appId: "1:134169095453:web:6009b7b4cscdcscdcscdscsc53380237945f4",
 };
 
 // Initialize Firebase
@@ -18,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 // Export Auth and Google Provider
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
